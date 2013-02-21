b6f8b244122b8489182391df1ec420d530107bde
diegomvh <diegomvh@gmail.com>
2013-02-21 18:48:00 -0300

    change for new menu

--- imageview/viewers.py
+++ imageview/viewers.py
@@ -94,7 +94,7 @@ class PMXImageViewer(QtGui.QWidget, PMXBaseEditor):
             print "No es una imagen valida"
         
     @classmethod
-    def contributeToMainMenu(cls, addonsClasses):
+    def contributeToMainMenu(cls):
         return { "Image": { 'title': "Images", 
                             'items': [ 
                             {   'title': "Convert To ASCII",
